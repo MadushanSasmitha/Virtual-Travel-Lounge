d292e05b5d2712ed3b6710f87835bea9df582b08
Madushan Sasmitha <115541177+MadushanSasmitha@users.noreply.github.com>
2025-11-25 18:02:26 +0530

    Add files via upload

---
+++ Virtual Travel LoungeTests/Virtual_Travel_LoungeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Virtual_Travel_LoungeTests.swift
+//  Virtual Travel LoungeTests
+//
+//  Created by STUDENT on 2025-11-24.
+//
+
+import Testing
+@testable import Virtual_Travel_Lounge
+
+struct Virtual_Travel_LoungeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Virtual Travel LoungeUITests/Virtual_Travel_LoungeUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Virtual_Travel_LoungeUITestsLaunchTests.swift
+//  Virtual Travel LoungeUITests
+//
+//  Created by STUDENT on 2025-11-24.
+//
+
+import XCTest
+
+final class Virtual_Travel_LoungeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
